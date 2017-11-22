304876dfcf07183ef19664dcd4954ab5eb1763c2
Tomas Dvorak <todvora@gmail.com>
2017-11-22 10:59:34 +0100

    append permalink to each error message

--- graylog-rocketchat.hooks.js
+++ graylog-rocketchat.hooks.js
@@ -1,9 +1,19 @@
 /* exported Script */
 /* globals console, _, s */
 
+const GRAYLOG_URL = 'https://graylog.example.com';
+
+const createPermalink = (m) => {
+    return `[Display complete message](${GRAYLOG_URL}/messages/${m.index}/${m.id})`
+};
+
+const formatMessage = (m) => {
+return m.message + '\n' + createPermalink(m);
+};
+
 const parseText = function (result) {
     if(result.matching_messages.length > 0) {
-        return result.matching_messages.map(m => m.message).join("\n\n");
+        return result.matching_messages.map(formatMessage).join("\n\n");
     } else {
         return result.result_description;
     }
--- test/graylog-rocketchat.hooks.test.js
+++ test/graylog-rocketchat.hooks.test.js
@@ -19,5 +19,9 @@ test('Handles dummy alert', () => {
 
 test('Handles error alert', () => {
     const response = new Script().process_incoming_request(getRequest('error-alert.json'));
-    expect(response.content.text).toBe('A global exception on URL http://www.example.com, headers: {Cookie=__cfduid=dd00f87ca1571f030cd86dc107ce0fbee1502342427; language=ro; JSESSIONID=example1rw0aif2vail73dck6kkzx1hk.example1; searches=1; gender=; birthyear=, CF');
+    const expected = [
+        'A global exception on URL http://www.example.com, headers: {Cookie=__cfduid=dd00f87ca1571f030cd86dc107ce0fbee1502342427; language=ro; JSESSIONID=example1rw0aif2vail73dck6kkzx1hk.example1; searches=1; gender=; birthyear=, CF',
+        '[Display complete message](https://graylog.example.com/messages/graylog_7/f7bc4d31-cf47-11e7-84b1-0242ac120004)'
+    ];
+    expect(response.content.text.split('\n')).toEqual(expected);
 });
