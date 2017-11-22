6eba1dd90edc0c34dbdf172d096450ab41eb44dc
Tomas Dvorak <todvora@gmail.com>
2017-11-22 07:05:32 +0100

    handle error event with messages

--- graylog-rocketchat.hooks.js
+++ graylog-rocketchat.hooks.js
@@ -1,10 +1,19 @@
 /* exported Script */
 /* globals console, _, s */
+
+const parseText = function (result) {
+    if(result.matching_messages.length > 0) {
+        return result.matching_messages.map(m => m.message).join("\n\n");
+    } else {
+        return result.result_description;
+    }
+};
+
 class Script {
     process_incoming_request({ request }) {
         return {
             content:{
-                text: request.content.check_result.result_description
+                text: parseText(request.content.check_result)
             }
         };
     }
--- test/graylog-rocketchat.hooks.test.js
+++ test/graylog-rocketchat.hooks.test.js
@@ -16,3 +16,8 @@ test('Handles dummy alert', () => {
     const response = new Script().process_incoming_request(getRequest('dummy-alert.json'));
     expect(response.content.text).toBe('Dummy alert to test notifications');
 });
+
+test('Handles error alert', () => {
+    const response = new Script().process_incoming_request(getRequest('error-alert.json'));
+    expect(response.content.text).toBe('A global exception on URL http://www.example.com, headers: {Cookie=__cfduid=dd00f87ca1571f030cd86dc107ce0fbee1502342427; language=ro; JSESSIONID=example1rw0aif2vail73dck6kkzx1hk.example1; searches=1; gender=; birthyear=, CF');
+});
